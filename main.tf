67ab6229ae3c9d7600fc7460ed7fdc4493467e46
vboxuser <vboxuser@vbox.myguest.virtualbox.org>
2025-06-17 01:00:29 +0200

    destroy no cost hhhhhh

--- main.tf
+++ main.tf
@@ -38,16 +38,16 @@ provider "aws" {
 
 # Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0a85dfbb3e308f782"
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-0c7c4e3c6b4941f0f"
+#   instance_type = "t2.micro"
+#   subnet_id = "subnet-0a85dfbb3e308f782"
 
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance"
+#   }
+# }
 
-resource "aws_iam_user" "test_user_99" {
-  name = "test-user-99"  
-}
+# resource "aws_iam_user" "test_user_99" {
+#   name = "test-user-99"  
+# }
